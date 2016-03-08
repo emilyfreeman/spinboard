4a97e5a64ee9014d1edbd14d7df5ef92b1ee85ad
Emily Dowdle <emily@editingemily.com>
2016-03-08 10:37:20 -0700

    model tests

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create link with title and url" do
+    link = Link.create(title: "Thing", url: "http://google.com")
+    assert link.valid?
+  end
+
+  test "link defaults read value to false" do
+    link = Link.create(title: "Thing", url: "http://google.com")
+    assert_equal link.read?, false
+  end
+
+  test "cannot create link without title" do
+    link = Link.create(url: "http://google.com")
+    refute link.valid?
+  end
+
+  test "cannot create link without url" do
+    link = Link.create(title: "thing")
+    refute link.valid?
+  end
+
+  test "cannot create link without valid url" do
+    link = Link.create(title: "thing", url: "wrong!")
+    refute link.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create user with title and password" do
+    user = User.create(email: "email@emily.com", password: "password")
+    assert user.valid?
+  end
+
+  test "cannot create user without email" do
+    user = User.create(password: "password")
+    refute user.valid?
+  end
+
+  test "cannot create user without password" do
+    user = User.create(email: "email@emily.com")
+    refute user.valid?
+  end
+
+  test "user can have many links" do
+    user = User.create!(email: "email@emily.com", password: "password")
+    user.links.create!(title: "wahoo", url: "http://google.com")
+
+    assert_equal user.links.count, 1
+  end
 end
