36540bb8d19f3f3ea93ddc811287d3efbe672b83
Emily Dowdle <emily@editingemily.com>
2016-03-08 08:34:57 -0700

    Update models with validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,6 @@
 class Link < ActiveRecord::Base
   belongs_to :user
+  validates :url, :url => true,
+                  presence: true
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :links
+
+  validates :email, presence: true,
+                    uniqueness: true
+
+  validates :password, presence: true
+
+  def password_match(params)
+    params[:password] == params[:confirm_password]
+  end
 end
